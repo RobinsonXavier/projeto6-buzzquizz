e2e186b7f8f6c151040c955f066b4b2238b97b8b
Hugo Gomes <gomes.hugo00@gmail.com>
2022-07-01 19:06:42 -0300

    Percorrer entre as telas

--- scripts/script.js
+++ scripts/script.js
@@ -80,18 +80,65 @@ getQuiz();
 
 
 
-// --------------------------------------------------------------------------------------
+// --------------------------------------------------------------------------------------------------------------------
 
-let $btn_create_quiz_1 = document.querySelector('.btn-dashed')
-let $btn_create_quiz_2 = document.querySelector('.btn_circle') 
-let $visualize_quizz = document.querySelector('.visualize_quizz')
-let $create_quizz = document.querySelector('.create_quizz')
+const $visualize_quizz = document.querySelector('.visualize_quizz')
+const $create_quizz = document.querySelector('.create_quizz')
 
-$btn_create_quiz_1.addEventListener('click', create_quizz)
-$btn_create_quiz_2.addEventListener('click', create_quizz)
+const $start_create_container = document.querySelector('.start_create_container')
+const $create_question_container = document.querySelector('.create_question_container')
+const $decide_level_container = document.querySelector('.decide_level_container')
+const $quizz_done_container = document.querySelector('.quizz_done_container')
 
-function create_quizz(){
+const $btn_create_quiz_1 = document.querySelector('.btn-dashed')
+const $btn_create_quiz_2 = document.querySelector('.btn_circle') 
+const $btn_create_question = document.querySelector('.btn-create-question')
+const $btn_decide_level = document.querySelector('.btn-decide-level') 
+const $btn_finalize_quizz = document.querySelector('.btn-finalize-quizz') 
+const $btn_quizz_done = document.querySelector('.btn-quizz-done')
+const $btn_back_home = document.querySelector('.btn-back-home')
+
+$btn_create_quiz_1.addEventListener('click', start_create_quizz)
+$btn_create_quiz_2.addEventListener('click', start_create_quizz)
+
+$btn_create_question.addEventListener('click', create_question)
+$btn_decide_level.addEventListener('click', decide_level)
+$btn_finalize_quizz.addEventListener('click', finalize_quizz)
+
+$btn_quizz_done.addEventListener('click', acess_quizz)
+$btn_back_home.addEventListener('click', back_home)
+
+function start_create_quizz(){
 
     $visualize_quizz.classList.add('invisible')
     $create_quizz.classList.remove('invisible')
 }
+
+function create_question(){
+
+    $start_create_container.classList.add('invisible')
+    $create_question_container.classList.remove('invisible')
+}
+
+function decide_level(){
+
+    $create_question_container.classList.add('invisible')
+    $decide_level_container.classList.remove('invisible')
+}
+
+function finalize_quizz(){
+    $decide_level_container.classList.add('invisible')
+    $quizz_done_container.classList.remove('invisible')
+}
+
+function acess_quizz(){
+    $quizz_done_container.classList.add('invisible')
+    $visualize_quizz.classList.remove('invisible')
+}
+
+function back_home(){
+    $quizz_done_container.classList.add('invisible')
+    $visualize_quizz.classList.remove('invisible')
+}
+
+// -----------------------------------------------------------------------------------------------------------------
