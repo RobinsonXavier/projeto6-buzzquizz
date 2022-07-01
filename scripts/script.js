eec63da6d1d6e894c0fba65e74232bcef833a7fc
Hugo Gomes <gomes.hugo00@gmail.com>
2022-07-01 15:31:22 -0300

    Adição de evento aos botões para criar quizz

--- scripts/script.js
+++ scripts/script.js
@@ -35,8 +35,8 @@ let listOfQuizz = () => {
     const displayOn = document.querySelector(".visualize_quizz");
     const listOfQuizz = document.querySelector(".allQuizz_content");
 
-    // displayOn.classList.remove("visualize_quizz");
-    // displayOn.classList.add("spot");
+    displayOn.classList.remove("visualize_quizz");
+    displayOn.classList.add("spot");
 
 
     for(let i = 0; i < quizzList.length; i++) {
@@ -47,7 +47,7 @@ let listOfQuizz = () => {
                                             <img src="${quizzList[i].image}" alt="">
                                         </div>
                                         <h3>${quizzList[i].title}</h3>
-                                        <input type="hidden" value="${i}">
+                                        <input type="hidden" value="${quizzList[i].id}">
                                     </div>`;
     }
 
@@ -74,3 +74,24 @@ let clickQuizz = (element) => {
 
 
 getQuiz();
+
+
+
+
+
+
+// --------------------------------------------------------------------------------------
+
+let $btn_create_quiz_1 = document.querySelector('.btn-dashed')
+let $btn_create_quiz_2 = document.querySelector('.btn_circle') 
+let $visualize_quizz = document.querySelector('.visualize_quizz')
+let $create_quizz = document.querySelector('.create_quizz')
+
+$btn_create_quiz_1.addEventListener('click', create_quizz)
+$btn_create_quiz_2.addEventListener('click', create_quizz)
+
+function create_quizz(){
+
+    $visualize_quizz.classList.add('invisible')
+    $create_quizz.classList.remove('invisible')
+}
